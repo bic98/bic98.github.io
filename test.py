a1fee302f7cb8bd00b7ef9c0f58cf483b51f6c09
bic98 <bic7885@gmail.com>
2025-04-19 16:36:59 +0900

    .

--- test.py
+++ test.py
@@ -37,5 +37,70 @@ def policy_eval(pi, V, env, gamma=0.9, threshold=1e-5):
     return V
 
 
-V = policy_eval(pi, V, env)
-env.render_v(V, pi)
+def greedy_policy(V, env, gamma=0.9):
+    pi = {}
+    for state in env.states():
+        action_values = {}
+        for action in env.actions():
+            next_state = env.next_state(state, action)
+            r = env.reward(state, action, next_state)
+            action_values[action] = r + gamma * V[next_state]
+        max_action = max(action_values, key=lambda a: action_values[a])
+        action_probs = {a: 0.0 for a in env.actions()}
+        action_probs[max_action] = 1.0
+        pi[state] = action_probs
+    return pi
+
+
+def policy_iter(env, gamma, threshold=1e-4, is_render=False):
+    pi = defaultdict(lambda: {0: 0.25, 1: 0.25, 2: 0.25, 3: 0.25})
+    V = defaultdict(lambda: 0.0)
+    while True:
+        V = policy_eval(pi, V, env, gamma, threshold)
+        new_pi = greedy_policy(V, env, gamma)
+        if is_render:
+            env.render_v(V, pi)
+        if new_pi == pi:
+            break
+        pi = new_pi
+    return pi, V
+
+
+def value_iter_onestep(V, env, gamma=0.9):
+    for state in env.states():
+        if state == env.goal_state:
+            V[state] = .0
+            continue
+        action_values = []
+        for action in env.actions():
+            next_state = env.next_state(state, action)
+            r = env.reward(state, action, next_state)
+            value = r + gamma * V[next_state]
+            action_values.append(value)
+        V[state] = max(action_values)
+    return V
+
+
+def value_iter(V, env, gamma, threshold=1e-3, is_render=True):
+    while True:
+        if is_render:
+            env.render_v(V)
+        old_V = V.copy()
+        V = value_iter_onestep(V, env, gamma)
+        delta = 0
+        for state in V.keys():
+            t = abs(old_V[state] - V[state])
+            if delta < t:
+                delta = t
+        if delta < threshold:
+            break
+    return V
+
+
+if __name__ == "__main__":
+    env = GridWorld()
+    V = defaultdict(lambda: 0.0)
+    gamma = 0.9
+    V = value_iter(V, env, gamma, threshold=1e-3, is_render=True)
+    pi = greedy_policy(V, env, gamma)
+    env.render_v(V, pi)
